8a1162259308de9afe1a5f51804d009cc6b513be
Nycollas <nhhenich@gmail.com>
2024-08-03 21:08:36 -0300

    initial

---
+++ basic/ex01.js
@@ -0,0 +1,6 @@
+// Crie duas variáveis, a e b, e atribua a elas os valores 5 e 10, respectivamente. Em seguida, exiba a soma dessas variáveis no console.
+
+let a = 5;
+let b = 10;
+
+console.log(a + b)
---
+++ basic/ex02.js
@@ -0,0 +1,3 @@
+// Escreva um código que verifique se um número é par ou ímpar. Exiba "Par" ou "Ímpar" no console.
+
+let nome = prompt('Qual o seu nome:')
